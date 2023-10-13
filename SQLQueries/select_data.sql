5ee6085119afa49fd18e23194dcaf85569bf73bb
YoWhatUpC0dy <117550027+YoWhatUpC0dy@users.noreply.github.com>
2023-10-13 06:13:53 -0400

    Add files via upload

---
+++ SQLQueries/select_data.sql
@@ -0,0 +1,6 @@
+-- SQL Query to Select Employee Information
+-- In this SQL script, I retrieve employee data from the 'Employees' table.
+-- You can see the real data in action.
+-- This query returns details about employees with their IDs, first names, last names, and hire dates.
+SELECT EmployeeID, FirstName, LastName, HireDate
+FROM Employees;
---
+++ SQLQueries/update_records.sql
@@ -0,0 +1,8 @@
+-- SQL Query to Update Employee Hire Dates
+-- In this SQL script, I would update the hire dates of employees in the 'Employees' table.
+-- You can observe real changes taking place.
+-- This query modifies the hire date of employees with an ID greater than 100.
+-- Feel free to adapt this script to reflect actual data manipulation.
+UPDATE Employees
+SET HireDate = '2023-09-30'
+WHERE EmployeeID > 100;
